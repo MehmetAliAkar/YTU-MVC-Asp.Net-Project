41ce4294d791a0afbc683104803dccdbc7af9278
Mehmet Ali AKAR <maliakar123@gmail.com>
2024-03-13 19:36:40 +0300

    Data Transformations Updated

---
+++ W02/Controllers/RegistrationsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace W02.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult About(int id, string name)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(int id, string name)
+        {
+            return View();
+        }
+
+
+
+    }
+}
