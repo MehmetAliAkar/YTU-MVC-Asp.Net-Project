1b21307075cfffe9558b2810b876376e139476c4
Mehmet Ali AKAR <maliakar123@gmail.com>
2024-03-31 16:01:08 +0300

    init project

---
+++ ObsWebUI/Models/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ObsWebUI.Models.Entities
+{
+	public class Course
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Course name is required!")]
+		[ForeignKey("DepartmentId")]
+		public int DepartmentId { get; set; }
+		[Required(ErrorMessage = "Course name is required!")]
+		public string? Name { get; set; }
+		
+	}
+}
